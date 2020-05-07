d2642e4988fdf7c52afbd99dde737ed3d1b97dc1
dhilhanop <62800990+dhilhanop@users.noreply.github.com>
2020-05-08 00:26:21 +0700

    Add files via upload

---
+++ modules/admin/views/_partials/sidebar.php
@@ -0,0 +1,52 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo base_url('admin/dashboard'); ?>">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="far fa-smile-beam"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3"><?php echo ucfirst($this->session->userdata('username')); ?></div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url('admin/dashboard'); ?>">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Product Management
+      </div>
+
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url('admin/products'); ?>">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Produk</span></a>
+      </li>
+     
+
+  
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
